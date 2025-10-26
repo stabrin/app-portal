315ea7c2b4f42d680631da7a4a945c159fa04b2c
Sergey <sergey@tabrin.ru>
2025-10-26 12:31:30 +0300

    16

--- desktop-app/src/admin_ui.py
+++ desktop-app/src/admin_ui.py
@@ -1044,7 +1044,7 @@ class AdminWindow(tk.Tk):
             "QR: Конфигурация сервера": json.dumps({"error": "not applicable"}),
             "QR: Конфигурация рабочего места": json.dumps({"error": "not applicable"}),
             "ap_workplaces.warehouse_name": "Тест DataMatrix (из БД)",
-            "ap_workplaces.workplace_number": 0
+            "ap_workplaces.workplace_number": "0" # ИСПРАВЛЕНИЕ: Преобразуем в строку, чтобы избежать ошибки 'int' object has no attribute 'isdigit'
         }
 
         # Вызываем нашу стандартную процедуру печати с предпросмотром.
