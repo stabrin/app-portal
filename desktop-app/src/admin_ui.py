8d5cafc1863ddbc80a4dc675526b9eaad2d58663
Sergey <sergey@tabrin.ru>
2025-11-06 05:38:51 +0300

    3

--- desktop-app/src/admin_ui.py
+++ desktop-app/src/admin_ui.py
@@ -1343,9 +1343,9 @@ class AdminWindow(tk.Tk):
                 'import': service.process_marking_scenarios_import
             },
             columns={
-                'id': ('ID', 50, 'center'),
-                'name': ('Название сценария', 300, 'w'),
-                'scenario_data': ('Параметры (JSON)', 600, 'w')
+                'id': ('ID', 10, 'center'),
+                'name': ('Название сценария', 200, 'w'),
+                'scenario_data': ('Параметры (JSON)', 800, 'w')
             },
             pk_field='id',
             editor_class=ScenarioEditorDialog # Используем кастомный редактор
