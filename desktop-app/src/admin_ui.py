c8cae2beaf949eea82a386d03620c6f1e2fa6ebc
Sergey <sergey@tabrin.ru>
2025-11-15 14:36:33 +0300

    1

--- desktop-app/src/admin_ui.py
+++ desktop-app/src/admin_ui.py
@@ -1485,9 +1485,6 @@ class ApiIntegrationFrame(ttk.Frame):
         self.split_runs_btn = ttk.Button(flow_panel, text="Разбить на тиражи", command=self._split_runs)
         self.split_runs_btn.pack(side=tk.LEFT, padx=2)
         self.prepare_json_btn = ttk.Button(flow_panel, text="Подготовить JSON", command=self._prepare_json)
-        # --- НОВЫЙ БЛОК: Кнопка для подписания ЭЦП ---
-        self.sign_and_send_btn = ttk.Button(flow_panel, text="Подписать и отправить", command=self._sign_with_eds)
-        # Кнопка будет показана/скрыта в _update_buttons_state
         self.prepare_json_btn.pack(side=tk.LEFT, padx=2)
         self.download_codes_btn = ttk.Button(flow_panel, text="Скачать коды", command=self._download_codes)
         self.download_codes_btn.pack(side=tk.LEFT, padx=2)
@@ -1526,15 +1523,13 @@ class ApiIntegrationFrame(ttk.Frame):
 
             # --- ИЗМЕНЕНИЕ: Управляем видимостью кнопок, а не только состоянием ---
             # Прячем все кнопки и показываем только нужные для текущего статуса
-            for btn in [self.request_codes_btn, self.sign_and_send_btn, self.split_runs_btn, self.prepare_json_btn]:
+            for btn in [self.request_codes_btn, self.split_runs_btn, self.prepare_json_btn]:
                 btn.pack_forget()
 
             if not api_status:
                 self.request_codes_btn.pack(side=tk.LEFT, padx=2)
             elif api_status == 'Запрос создан':
-                # Вместо "Разбить на тиражи" теперь показываем "Подписать"
-                self.sign_and_send_btn.pack(side=tk.LEFT, padx=2)
-            elif api_status == 'Запрос подписан': # Новый гипотетический статус
+                # После создания запроса сразу даем возможность разбить на тиражи
                 self.split_runs_btn.pack(side=tk.LEFT, padx=2)
 
             # Кнопка "Подготовить JSON" активна, если статус 'Тиражи созданы'
