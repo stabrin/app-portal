cea5b81088f2bffb4d10f34818c61faf70e8d500
Sergey <sergey@tabrin.ru>
2025-11-30 14:32:36 +0300

    1

--- desktop-app/src/admin_ui_qt.py
+++ desktop-app/src/admin_ui_qt.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import (
     QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTableWidget,
     QTableWidgetItem, QMessageBox, QApplication, QLabel, QFileDialog, QTextEdit,
     QLineEdit, QHeaderView, QDateEdit, QDialog, QFormLayout, QComboBox, QSplitter, QTabWidget, QProgressDialog,
+    QGroupBox, QRadioButton, QSpinBox,
     QInputDialog, QTreeWidget, QTreeWidgetItem, QStackedWidget, QAbstractItemView
 )
 from PySide6.QtCore import Qt, Slot, QDate, QTimer, QThread, Signal, QObject
