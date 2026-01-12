14b04a78ce1a179b880f3e5e0025253af16b9a24
Sergey <sergey@tabrin.ru>
2026-01-12 07:38:34 +0300

    Сопоставление кодов

--- setup_oper.py
+++ setup_oper.py
@@ -6,7 +6,7 @@ import psycopg2
 
 # --- ПАРАМЕТРЫ ---
 APP_NAME = "TildaKod_WS"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 DESCRIPTION = "Приложение для печати этикеток и сканирования"
 AUTHOR = "Tabrin Sergey"
 UPGRADE_CODE = "{688cf803-e16e-40c8-a135-367020ac7df8}"
